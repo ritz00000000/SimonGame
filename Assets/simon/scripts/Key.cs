5e865ad82c421d4891f45321ef7d9e97302a3e06
ritz00000000 <ritz00000000@hotmail.com>
2018-07-02 14:07:04 -0400

    simple simon game

---
+++ Assets/simon/scripts/Key.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    public Animator anim;
+    public Material idleMat;
+    public Material downMat;
+    
+    public int numGen=99;
+
+    public HelloARController myController;
+    public delegate void ClickEV(int number);
+    public event ClickEV onClick;
+    public AudioSource aloha;
+    public Renderer myR;
+
+	private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        aloha = GetComponent<AudioSource>();
+        myR = GetComponent<Renderer>();
+        myR.enabled = true;
+    }
+	
+	
+    private void OnMouseDown()
+    {
+        if (myController.playable == true)
+        {
+            anim.SetBool("onclick", true);
+            aloha.Play();
+            if (myController.player)
+            {
+                onClick(numGen);
+            }
+        }
+    }
+    private void OnMouseUp()
+    {
+        anim.SetBool("onclick", false);
+
+    }
+}
